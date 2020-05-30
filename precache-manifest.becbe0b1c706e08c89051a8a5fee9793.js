d007c5565e5f7c91755232707f4a4afe926f3bcc
Ladanov Nikolay <ladanov_n@mail.ru>
2020-05-31 01:25:13 +0300

    Updates

---
+++ precache-manifest.becbe0b1c706e08c89051a8a5fee9793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4c2c53143c21f0bc7dcd2570e1cf9f",
+    "url": "/oxford/index.html"
+  },
+  {
+    "revision": "1d74720928b20419c318",
+    "url": "/oxford/static/css/main.02dcbfa0.chunk.css"
+  },
+  {
+    "revision": "83a1cba4e2dde68b2906",
+    "url": "/oxford/static/js/2.34839e5a.chunk.js"
+  },
+  {
+    "revision": "1d74720928b20419c318",
+    "url": "/oxford/static/js/main.b2a6bf19.chunk.js"
+  },
+  {
+    "revision": "7cdd3a5f170de5f99a2e",
+    "url": "/oxford/static/js/runtime-main.69507663.js"
+  }
+]);
